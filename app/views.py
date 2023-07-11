3d9f16b6c3c90df60196e56558e7a3fdfda05588
immanelg <dmitryborovicky@gmail.com>
2023-07-11 22:58:33 +0300

    rm obsolete routes

--- app/views.py
+++ app/views.py
@@ -9,16 +9,3 @@ async def home(request: Request) -> Any:
     context = {"request": request, "user_id": request.cookies.get("user_id", "<unset>")}
     return templates.TemplateResponse("index.html", context)
 
-
-async def lobby_view(request: Request) -> Any:
-    context = {"request": request, "user_id": request.cookies.get("user_id", "<unset>")}
-    return templates.TemplateResponse("lobby.html", context)
-
-
-async def game_view(request: Request) -> Any:
-    context = {
-        "request": request,
-        "user_id": request.cookies.get("user_id"),
-        "game_id": request.path_params.get("game_id"),
-    }
-    return templates.TemplateResponse("game.html", context)
