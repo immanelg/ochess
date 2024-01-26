4c8eac901eea4e64d73f7a2c3ee045b360f39915
immanelg <119798691+immanelg@users.noreply.github.com>
2024-01-26 14:27:15 +0300

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,37 @@
 import os
 from typing import Generator, AsyncGenerator
 import asyncio
+
 import pytest
 import pytest_asyncio
 from async_asgi_testclient import TestClient
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql import text
+
 from app.main import app
 from app.database.database import engine
 
-@pytest_asyncio.fixture(autouse=True, scope="session")
-async def setup_database():
-    # alembic.config.main(argv=['--raiseerr', 'upgrade', 'head'])  # causes nested event loop?
-    os.system("alembic upgrade head")
-    yield
-    async with engine.begin() as conn:
-        await conn.execute(text("drop schema public cascade"))
-        await conn.execute(text("create schema public"))
 
-
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def event_loop() -> Generator[asyncio.AbstractEventLoop, None, None]:
-    loop = asyncio.get_event_loop_policy().new_event_loop()
+    loop = asyncio.new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest_asyncio.fixture(autouse=True, scope="session")
+async def setup_database():
+    # TODO: use alembic 
+    from app.database.models import Base
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        pass
+
+    await engine.dispose()
 
 
 @pytest_asyncio.fixture
@@ -36,7 +44,7 @@ async def client() -> AsyncGenerator[TestClient, None]:
 
 
 @pytest_asyncio.fixture(scope="session")
-async def session(setup_database) -> AsyncGenerator[AsyncSession, None]:
+async def session() -> AsyncGenerator[AsyncSession, None]:
     from app.database.database import get_session
     async with get_session() as session:
         yield session
