ecf285ee106b0bffad5dcfe79bd6460475f790d4
immanelg <119798691+immanelg@users.noreply.github.com>
2024-01-28 16:24:44 +0300

    DB healthcheck

--- app/lifespan.py
+++ app/lifespan.py
@@ -1,15 +1,23 @@
 from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+from sqlalchemy import text
 
 from starlette.applications import Starlette
 
 from app.resources import broadcast
+from app.database.database import engine
 
 
 @asynccontextmanager
-async def lifespan(app: Starlette):
+async def lifespan(app: Starlette) -> AsyncGenerator[None, None]:
     try:
+        async with engine.begin() as conn: 
+            # healthcheck
+            await conn.execute(text("SELECT 1"))
         await broadcast.connect()
         yield
-    except Exception as e:
+
+    except Exception:
+        await engine.dispose()
         await broadcast.disconnect()
-        raise e
+        raise 
