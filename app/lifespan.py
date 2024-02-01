527e2e77c513059286590b57bdcec5ca6896c488
immanelg <119798691+immanelg@users.noreply.github.com>
2024-02-01 22:43:49 +0300

    More logs because logging is cool

--- app/lifespan.py
+++ app/lifespan.py
@@ -5,16 +5,21 @@ from sqlalchemy import text
 from starlette.applications import Starlette
 
 from app.database.database import engine
+from app.logging import logger
 from app.resources import broadcast
 
 
 @asynccontextmanager
 async def lifespan(app: Starlette) -> AsyncGenerator[None, None]:
     try:
+        logger.info("DB healthcheck")
         async with engine.begin() as conn:
-            # healthcheck
             await conn.execute(text("SELECT 1"))
+
+        logger.info("connect broadcast")
         await broadcast.connect()
+
+        logger.info("starting app")
         yield
 
     except Exception:
